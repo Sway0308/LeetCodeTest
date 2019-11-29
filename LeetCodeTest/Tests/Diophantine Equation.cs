363d5d7230bcdc0e248a70b2b9d7298f44e180b9
Sway0308 <sway0308@gmail.com>
2019-11-29 10:39:49 +0800

    add new test

---
+++ LeetCodeTest/Tests/Diophantine Equation.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeTest.Tests
+{
+    class Diophantine_Equation : ISolution
+    {
+        public string Method()
+        {
+            return solEquaStr(90005).ToString();
+        }
+
+        string solEquaStr(long n)
+        {
+            var x = new Queue<string>();
+            long mid = (n + 1) / 2;
+            var isExist = false;
+            for (long i = mid; i >= 0; i--)
+            {
+                for (long j = mid; j >= 0; j--)
+                {
+                    if ((i - 2 * j) * (i + 2 * j) == n)
+                    {
+                        x.Enqueue((isExist ? ", " : "") + $"[{i}, {j}]");
+                        isExist = true;
+                        break;
+                    }
+                }
+            }
+
+            return $"[{string.Join("", x)}]";
+        }
+    }
+}
---
+++ LeetCodeTest/Tests/FindArrayPacking.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeTest.Tests
+{
+    class FindArrayPacking : ISolution
+    {
+        public string Method()
+        {
+            return arrayPacking(new List<int> { 30, 29, 18, 44 }).ToString();
+        }
+
+        static int arrayPacking(List<int> a)
+        {
+            int result = 0;
+            for (int index = 0; index < a.Count; index++)
+            {
+                if (a[index] > 256 || a[index] < 0)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                else
+                {
+                    result += a[index] << 8 * index;
+                }
+            }
+            return result;
+        }
+    }
+}
