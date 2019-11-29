9ef317d9d30913c85fa42b65e09c6b3583abda1f
Sway0308 <sway0308@gmail.com>
2019-11-29 09:36:26 +0800

    new test

---
+++ LeetCodeTest/Tests/FindFirstNonRepeating.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeTest.Tests
+{
+    class FindFirstNonRepeating : ISolution
+    {
+        public string Method()
+        {
+            var ans = firstNonRepeating("sTreSs");
+            return ans;
+        }
+
+        string firstNonRepeating(String str)
+        {
+            var compareStr = str.ToLower();
+            var arr = new int[256];
+            for (int i = 0; i < arr.Length; i++)
+                arr[i] = -1;
+
+            for (int i = 0; i < compareStr.Length; i++)
+            {
+                if (arr[compareStr[i]] == -1)
+                    arr[compareStr[i]] = i;
+                else
+                    arr[compareStr[i]] = -2;
+            }
+
+            int res = int.MaxValue;
+            for (int i = 0; i < arr.Length; i++)
+
+                // If this character occurs  
+                // only once and appears before 
+                // the current result, then  
+                // update the result 
+                if (arr[i] >= 0)
+                    res = Math.Min(res, arr[i]);
+
+            if (res == int.MaxValue)
+                return "";
+            return str.Substring(res, 1);
+        }
+    }
+}
---
+++ LeetCodeTest/Tests/FindSumOfThousands.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeTest.Tests
+{
+    class FindSumOfThousands : ISolution
+    {
+        public string Method()
+        {
+            return SumOfThousands(9000).ToString();
+        }
+
+        int SumOfThousands(int num)
+        {
+            var a = num / 1000;
+            var sum = 0;
+            for (int i = 1; i <= a; i++)
+            {
+                sum += i;
+            }
+            return sum * 1000;
+        }
+    }
+}
